3109295c91725d01365efe267b46a48c32d6d787
john1663 <jrw1663@gmail.com>
2020-03-31 20:23:49 +0100

    Added test script

---
+++ bin/test2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+print ("Hello World!")
+
+import datetime
+import yfinance as yf
+import openpyxl as xl
+import pandas as pd
+import json
+import logging
+
+my_timestamp = datetime.datetime.now().isoformat()
